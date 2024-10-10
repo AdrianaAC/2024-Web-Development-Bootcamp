c9614f9a5e9fb38978a1f5596c45970c55a2b699
Adriana Alves <adrianaalves098@gmail.com>
2024-10-10 01:07:45 +0100

    bootstrap components

---
+++ exercises.js
@@ -0,0 +1,8 @@
+//Table exercises: col - lesson nbr, lesson name, exercise desc, exercise link
+//
+//11.1 - Bootstrap layout
+// https://appbrewery.github.io/bootstrap-layout/
+//
+//Table links: col - lesson nbr, lesson name, link desc, link name, link
+//11.2 - Bootstrap components
+//https://getbootstrap.com/docs
