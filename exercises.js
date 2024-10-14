74eebb1813111648224144a0378015f50f894aa2
Adriana Alves <adrianaalves098@gmail.com>
2024-10-14 03:42:10 +0100

    TinDog prj

--- exercises.js
+++ exercises.js
@@ -2,6 +2,8 @@
 //
 //11.1 - Bootstrap layout
 // https://appbrewery.github.io/bootstrap-layout/
+//11.3 - TinDog
+//https://appbrewery.github.io/tindog/
 //
 //Table links: col - lesson nbr, lesson name, link desc, link name, link
 //11.2 - Bootstrap components
