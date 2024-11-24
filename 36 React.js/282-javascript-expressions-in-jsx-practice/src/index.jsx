90e11a1c10c0d6ecd26616a2a27810fc113d9c80
Adriana Alves <adrianaalves098@gmail.com>
2024-11-24 06:04:28 +0000

    282 jsx challenge solved

--- 36 React.js/282-javascript-expressions-in-jsx-practice/src/index.jsx
+++ 36 React.js/282-javascript-expressions-in-jsx-practice/src/index.jsx
@@ -1,14 +1,11 @@
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import { createRoot } from "react-dom/client";
+const MYNAME = "Consuela";
+const CURRENTYEAR = new Date().getFullYear();
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <p>Created by {MYNAME}.</p>
+    <p>Copyright {CURRENTYEAR}.</p>
+  </div>
+);
