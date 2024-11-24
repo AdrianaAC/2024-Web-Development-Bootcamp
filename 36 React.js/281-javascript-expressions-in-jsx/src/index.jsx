2fbe43e833051c187b045c3ea29a866e0302e25d
Adriana Alves <adrianaalves098@gmail.com>
2024-11-24 04:49:00 +0000

    Javascript Expressions in JSX & ES6 Template Literals

--- 36 React.js/281-javascript-expressions-in-jsx/src/index.jsx
+++ 36 React.js/281-javascript-expressions-in-jsx/src/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+const name = "Consuela";
+const sentence = "I hate cleaning";
+const hates = "cleaning";
+const number = 13;
+root.render(
+  <div>
+    <h1>Hello {`${name} who hates ${hates}`}</h1> {/* template literal, ES6 */}
+    <p>I am {name + " and " + sentence}</p>
+    <p>{sentence + " " } {sentence}</p>
+    <p>My lucky number is {number + 1}</p>
+    <p>Random number {Math.floor(Math.random() * 11)}</p>
+  </div>
+);
 
 // If you're running this locally in VS Code use the commands:
 // npm install
