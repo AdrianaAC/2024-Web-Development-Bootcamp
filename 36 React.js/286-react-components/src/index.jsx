bf3230e989380b4fccc0fe559f408ce361ece7ec
Adriana Alves <adrianaalves098@gmail.com>
2024-11-26 03:16:58 +0000

    286 final

--- 36 React.js/286-react-components/src/index.jsx
+++ 36 React.js/286-react-components/src/index.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import Title from "./Heading";
+import List from "./List";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
+    <Title />
+    <List />
+  </div>
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
