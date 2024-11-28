52b2d85e444325f3ae298002ed49dd5be5ce8f97
Adriana Alves <adrianaalves098@gmail.com>
2024-11-28 02:33:10 +0000

    288 final

--- 36 React.js/288-es6-import-export-and-modules/src/index.jsx
+++ 36 React.js/288-es6-import-export-and-modules/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pie, {doublePi, triplePi} from "./math.js";
+
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pie}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
