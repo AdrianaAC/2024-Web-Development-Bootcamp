31c31f847701442b9a4e4a186c6fb0ff40b4e45c
Adriana Alves <adrianaalves098@gmail.com>
2024-11-28 02:40:02 +0000

    289 final

--- 36 React.js/289-es6-import-export-practice/src/calculator.js
+++ 36 React.js/289-es6-import-export-practice/src/calculator.js
@@ -13,3 +13,5 @@ function subtract(n1, n2) {
 function divide(n1, n2) {
   return n1 / n2;
 }
+
+export { add, multiply, subtract, divide };
--- 36 React.js/289-es6-import-export-practice/src/index.jsx
+++ 36 React.js/289-es6-import-export-practice/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { add, multiply, subtract, divide } from "./math";
+import { add, multiply, subtract, divide } from "./calculator";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
