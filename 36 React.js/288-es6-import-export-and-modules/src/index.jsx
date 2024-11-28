bd8fea1e9be66c6eb5eb5d671cd873cc116f1869
Adriana Alves <adrianaalves098@gmail.com>
2024-11-28 01:05:58 +0000

    288 start

---
+++ 36 React.js/288-es6-import-export-and-modules/src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
