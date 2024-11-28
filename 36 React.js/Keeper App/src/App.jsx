342a2bd7b066c97e77e8b0defdb884085ea59167
Adriana Alves <adrianaalves098@gmail.com>
2024-11-28 04:36:30 +0000

    Keeper App - challenge 1 - solved

---
+++ 36 React.js/Keeper App/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ 36 React.js/Keeper App/src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>Content</p>
+    </div>
+  );
+}
