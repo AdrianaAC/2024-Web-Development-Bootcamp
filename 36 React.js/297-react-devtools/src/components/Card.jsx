113798c144f6602a35cb71d8a2495da8ea27dbfb
Adriana Alves <adrianaalves098@gmail.com>
2024-12-01 05:44:17 +0000

    297 final

--- 36 React.js/297-react-devtools/src/components/Card.jsx
+++ 36 React.js/297-react-devtools/src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar style="circle-img" imgSrc={props.img} imgAlt={props.name} />
       </div>
       <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
+        <Details className="info" telDetail={props.tel} />
+        <Details className="info" emailDetail={props.email} />
       </div>
     </div>
   );
