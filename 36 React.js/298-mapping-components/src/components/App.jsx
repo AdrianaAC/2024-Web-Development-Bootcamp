6592cb5d4b4bd6ebe87026b3b2cb3edaee7099c5
Adriana Alves <adrianaalves098@gmail.com>
2024-12-04 02:46:59 +0000

    298 final

--- 36 React.js/298-mapping-components/src/components/App.jsx
+++ 36 React.js/298-mapping-components/src/components/App.jsx
@@ -3,28 +3,22 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function App() {
+  let contactList = contacts.map((contact) => {
+    return (
+      <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+      />
+    );
+  });
+  console.log(contacts);
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contactList}
     </div>
   );
 }
