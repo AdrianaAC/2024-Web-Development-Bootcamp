4edb269137818fc6be2fc650ebdf1e98bfb8b501
Adriana Alves <adrianaalves098@gmail.com>
2024-12-09 13:05:19 +0000

    Keeper App - Challenge 2 solved

--- 36 React.js/Keeper App/src/App.jsx
+++ 36 React.js/Keeper App/src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
+
+  let notesList = notes.map(note => {
+    return <Note title={note.title} content={note.content}/>
+  });
+  
   return (
     <div>
       <Header />
-      <Note />
+      {notesList} 
       <Footer />
     </div>
   );
--- 36 React.js/Keeper App/src/components/Note.jsx
+++ 36 React.js/Keeper App/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Note() {
+export default function Note(props) {
   return (
     <div className="note">
-      <h1>Title</h1>
-      <p>Content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
