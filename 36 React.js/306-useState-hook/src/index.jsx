35e5afddf9d9d18c5783780bb7befefda823bd88
Adriana Alves <adrianaalves098@gmail.com>
2024-12-10 05:02:45 +0000

    306 start

---
+++ 36 React.js/306-useState-hook/src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div className="container">
+    <h1>0</h1>
+    <button>+</button>
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
