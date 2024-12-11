53dc56e5698bfbc17b10c3b05942b93829755577
Adriana Alves <adrianaalves098@gmail.com>
2024-12-11 03:19:30 +0000

    308 start

---
+++ 36 React.js/308-es6-destructuring/src/index.jsx
@@ -0,0 +1,29 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
