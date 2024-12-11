9ee6af70ad04efbd11b92dce5ffae26516e3df04
Adriana Alves <adrianaalves098@gmail.com>
2024-12-11 23:27:04 +0000

    310 final

--- 36 React.js/310-event-handling-in-react/src/components/App.jsx
+++ 36 React.js/310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
 import React from "react";
 
 function App() {
+  const [title, setTitle] = useState("Hello");
+  const [input, setInput] = useState("");
+  const [hover, setHover] = useState(false);
+
+  function handleSubmit() {
+    setTitle(`Hello, ${input}`);
+  }
+
+  function handleInput(event) {
+    setInput(event.target.value);
+  }
+
+  function handleHover(event) {
+    // event.target.style.backgroundColor = "black";
+    setHover(true);
+  }
+
+  function handleOut(event) {
+    // event.target.style.backgroundColor = "white";
+    setHover(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{title}</h1>
+      <input
+        type="text"
+        placeholder="What's your name?"
+        onChange={handleInput}
+      />
+      <button
+        onClick={handleSubmit}
+        onMouseOut={handleOut}
+        onMouseOver={handleHover}
+        style={{ backgroundColor: hover ? "black" : "white" }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
--- 36 React.js/310-event-handling-in-react/src/index.jsx
+++ 36 React.js/310-event-handling-in-react/src/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
