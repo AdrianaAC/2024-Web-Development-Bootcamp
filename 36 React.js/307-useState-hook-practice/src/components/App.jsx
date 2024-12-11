ba082a871179ec2928664401eed9ce00eca11d83
Adriana Alves <adrianaalves098@gmail.com>
2024-12-11 03:15:59 +0000

    307 final

--- 36 React.js/307-useState-hook-practice/src/components/App.jsx
+++ 36 React.js/307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    return setTime(new Date().toLocaleTimeString());
+  }
+
+  setInterval(getTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
