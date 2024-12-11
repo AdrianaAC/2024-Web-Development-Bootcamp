41a3195c91dd6cdd82ca736c7e3c1dc9ef867e4a
Adriana Alves <adrianaalves098@gmail.com>
2024-12-11 01:48:19 +0000

    306 final

--- 36 React.js/306-useState-hook/src/index.jsx
+++ 36 React.js/306-useState-hook/src/index.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+import { createRoot } from "react-dom/client";
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
