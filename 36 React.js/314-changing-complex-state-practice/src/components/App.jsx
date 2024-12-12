268526275107295ef0d1b8b21ddb037848a034ad
Adriana Alves <adrianaalves098@gmail.com>
2024-12-12 23:17:54 +0000

    314 final

--- 36 React.js/314-changing-complex-state-practice/src/components/App.jsx
+++ 36 React.js/314-changing-complex-state-practice/src/components/App.jsx
@@ -2,21 +2,54 @@ import React, { useState } from "react";
 
 function App() {
   const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
+    firstName: "",
+    lastName: "",
+    email: "",
   });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    if (name == "fName") {
+      setContact((prevValue) => {
+        return {
+          firstName: value,
+          lastName: prevValue.lastName,
+          email: prevValue.email,
+        };
+      });
+    } else if (name == "lName") {
+      setContact({...contact, lastName: value });
+    } else {
+      setContact({...contact, email: value });
+    }
+  }
 
   return (
     <div className="container">
       <h1>
-        Hello {contact.fName} {contact.lName}
+        Hello {contact.firstName} {contact.lastName}
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          value={contact.firstName}
+          onChange={handleChange}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lastName}
+          onChange={handleChange}
+        />
+        <input
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+          onChange={handleChange}
+        />
         <button>Submit</button>
       </form>
     </div>
@@ -24,3 +57,7 @@ function App() {
 }
 
 export default App;
+
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
