6965fd7f81e060cafb4bd1ed115e1c2ee442857b
Adriana Alves <adrianaalves098@gmail.com>
2024-12-12 01:05:42 +0000

    311 final

--- 36 React.js/311-react-forms/src/components/App.jsx
+++ 36 React.js/311-react-forms/src/components/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import React from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [title, setTitle] = useState("Hello");
+
+
+  function handleSubmit() {
+    setTitle("Hello " + input);
+  }
+
+  function handleChange(event) {
+    setInput(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{title}</h1>
+      <input type="text" placeholder="What's your name?" value={input} onChange={handleChange} />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
