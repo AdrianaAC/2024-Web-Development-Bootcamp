f843da79f6321a3faea6789c69dab5c783294d56
Adriana Alves <adrianaalves098@gmail.com>
2025-01-15 16:36:19 +0000

    updating sentence

--- 17 The Dicee Game/index.js
+++ 17 The Dicee Game/index.js
@@ -1,25 +1,22 @@
-function randomNumber1() {
-  let dice1 = Math.floor(Math.random() * 6) + 1;
-  console.log(dice1);
-  return dice1;
-}
-
-function randomNumber2() {
-  let dice2 = Math.floor(Math.random() * 6) + 1;
-  console.log(dice2);
-  return dice2;
-}
-
 function dice1Img() {
-  let dice1 = randomNumber1();
-  let dice1Img = "./images/dice" + dice1 + ".png";
-  return dice1Img;
+  let dice1 = Math.floor(Math.random() * 6) + 1;
+  return { dice1: dice1, src: `images/dice${dice1}.png` }; // Return both the dice value and the image source
 }
 
 function dice2Img() {
-  let dice2 = randomNumber2();
-  let dice2Img = "./images/dice" + dice2 + ".png";
-  return dice2Img;
+  let dice2 = Math.floor(Math.random() * 6) + 1;
+  return { dice2: dice2, src: `images/dice${dice2}.png` }; // Return both the dice value and the image source
 }
 
-export { dice1Img, dice2Img };
+function getWinner(dice1, dice2) {
+  let statement;
+  if (dice1 > dice2) {
+    statement = "Player 1 Wins";
+  } else if (dice2 > dice1) {
+    statement = "Player 2 Wins";
+  } else if (dice1 === dice2) {
+    statement = "Draw";
+  }
+  return statement;
+}
+export { dice1Img, dice2Img, getWinner };
