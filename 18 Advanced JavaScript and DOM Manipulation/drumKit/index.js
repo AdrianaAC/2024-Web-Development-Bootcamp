f8bbcc40ce7f36654d30a554915022841c809a6b
Adriana Alves <adrianaalves098@gmail.com>
2025-01-28 01:15:30 +0000

    139. Added background images to buttons . Add a swith case to handle sounds

--- 18 Advanced JavaScript and DOM Manipulation/drumKit/index.js
+++ 18 Advanced JavaScript and DOM Manipulation/drumKit/index.js
@@ -4,6 +4,39 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
+let dAudio = new Audio("sounds/snare.mp3");
+let wAudio = new Audio("sounds/crash.mp3");
+let aAudio = new Audio("sounds/tom-3.mp3");
+let sAudio = new Audio("sounds/kick-bass.mp3");
+let jAudio = new Audio("sounds/tom-1.mp3");
+let kAudio = new Audio("sounds/tom-2.mp3");
+let lAudio = new Audio("sounds/tom-4.mp3");
+
 function handleClick() {
-  alert("Hi there!");
+  let drumPart = this.innerHTML;
+  switch (drumPart) {
+    case "d":
+      dAudio.play();
+      break;
+    case "w":
+      wAudio.play();
+      break;
+    case "a":
+      aAudio.play();
+      break;
+    case "s":
+      sAudio.play();
+      break;
+    case "j":
+      jAudio.play();
+      break;
+    case "k":
+      kAudio.play();
+      break;
+    case "l":
+      lAudio.play();
+      break;
+    default:
+      console.log("Error");
+  }
 }
