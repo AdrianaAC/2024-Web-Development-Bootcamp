9327c142e9eead8c097e709a850856c01603a98d
Adriana Alves <adrianaalves098@gmail.com>
2025-02-17 17:10:38 +0000

    153. Manipulating Styles with JQuery

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -1,1 +1,5 @@
-$("h1").css("color", "red");
+const h1 = $("h1");
+h1.addClass("bigTitle");
+console.log(h1.hasClass("bigTitle"));
+h1.removeClass("bigTitle");
+console.log(h1.hasClass("bigTitle"));
