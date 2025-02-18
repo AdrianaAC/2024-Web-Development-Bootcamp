f651f84c0ebe8b8dda8ee7c62a1c00127711c2de
Adriana Alves <adrianaalves098@gmail.com>
2025-02-18 22:33:41 +0000

    154. Manipulating text with JQuery

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -3,3 +3,8 @@ h1.addClass("bigTitle");
 console.log(h1.hasClass("bigTitle"));
 h1.removeClass("bigTitle");
 console.log(h1.hasClass("bigTitle"));
+
+$("button").click(() => {
+  h1.html("<em>Hello</em>");
+  //   h1.text("I got clicked!");
+});
