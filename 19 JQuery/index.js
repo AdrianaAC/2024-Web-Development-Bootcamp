dfb9fbcda162e5f9763adb68d6aff64f93039377
Adriana Alves <adrianaalves098@gmail.com>
2025-02-19 00:23:04 +0000

    155. Manipulating attributes with JQuery

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -1,10 +1,14 @@
 const h1 = $("h1");
+const h2 = $("h2");
+const button = $("button");
+const a = $("a");
 h1.addClass("bigTitle");
 console.log(h1.hasClass("bigTitle"));
 h1.removeClass("bigTitle");
 console.log(h1.hasClass("bigTitle"));
 
-$("button").click(() => {
+button.click(() => {
   h1.html("<em>Hello</em>");
-  //   h1.text("I got clicked!");
+  h2.text("<em>I got clicked!<em>");
+  a.attr("href", "https://www.yahoo.com");
 });
