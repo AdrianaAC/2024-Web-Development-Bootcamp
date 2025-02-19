6cd34b929de2e75d51754edaf2d6c1bf4e046bcb
Adriana Alves <adrianaalves098@gmail.com>
2025-02-19 02:03:31 +0000

    156. Adding event listeners with JQuery

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -2,13 +2,31 @@ const h1 = $("h1");
 const h2 = $("h2");
 const button = $("button");
 const a = $("a");
-h1.addClass("bigTitle");
-console.log(h1.hasClass("bigTitle"));
-h1.removeClass("bigTitle");
-console.log(h1.hasClass("bigTitle"));
+const input = $("input");
+const p = $("p");
 
 button.click(() => {
-  h1.html("<em>Hello</em>");
-  h2.text("<em>I got clicked!<em>");
   a.attr("href", "https://www.yahoo.com");
+  h1.removeClass("bigTitle");
+});
+
+h1.click(() => {
+  h1.html("<em>Hello</em>");
+  h1.addClass("bigTitle");
+});
+
+h2.click(() => {
+  h2.text("<em>I got clicked!<em>");
+});
+
+h2.on("mouseover", () => {
+  h2.css("color", "red");
+});
+
+$(document).on("keydown", (event) => {
+  p.text(event.key);
+});
+
+input.keypress((event) => {
+  h1.text(event.key);
 });
