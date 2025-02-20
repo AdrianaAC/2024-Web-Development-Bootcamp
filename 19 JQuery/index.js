1ad652203639ca8ca4cc7b143b71b90014d03f59
Adriana Alves <adrianaalves098@gmail.com>
2025-02-20 00:30:27 +0000

    158. Website animations with JQuery

--- 19 JQuery/index.js
+++ 19 JQuery/index.js
@@ -7,20 +7,32 @@ const p = $("p");
 
 button.click(() => {
   a.attr("href", "https://www.yahoo.com");
-  h1.removeClass("bigTitle");
+  h1.removeClass("bigTitle").hide();;
+  h2.toggle();
+  p.fadeToggle();
+  a.animate({opacity: 0.5});
+
+  // Other methods:
+  //.fadeOut();
+  //.slideOut()
+  //.slideUp()
+  //.slideToggle()
 });
 
 h1.click(() => {
   h1.html("<em>Hello</em>");
   h1.addClass("bigTitle");
+  h2.remove();
 });
 
 h2.click(() => {
   h2.text("<em>I got clicked!<em>");
+  a.fadeIn();
 });
 
 h2.on("mouseover", () => {
   h2.css("color", "red");
+  h1.show();
 });
 
 $(document).on("keydown", (event) => {
@@ -35,4 +47,3 @@ p.before("<button>Before</button>");
 p.after("<button>After</button>");
 p.prepend("<button>Prepend</button>");
 p.append("<button>Append</button>");
-h2.remove();
