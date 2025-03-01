0f47638b3fd942403647d37b309999d3db5fac5d
Adriana Alves <adrianaalves098@gmail.com>
2025-03-01 04:08:15 +0000

    167. Added animations and sounds to buttons

--- 20 Challenge 2 - The Simon Game/game.js
+++ 20 Challenge 2 - The Simon Game/game.js
@@ -1,10 +1,17 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let clickedButton = $(".btn").attr("id");
+
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  console.log(gamePattern);
 }
 
+$(".btn").click(function () {
+  clickedButton = $(this).attr("id");
+  $(this).fadeOut(100).fadeIn(100);
+  let audio = new Audio("./sounds/" + clickedButton + ".mp3");
+  audio.play();
+});
