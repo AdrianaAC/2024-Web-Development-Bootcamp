70c746adf6c8562be5aa15ef0f02a5c4548f1e53
Adriana Alves <adrianaalves098@gmail.com>
2025-03-01 02:27:28 +0000

    165. Create a new pattern

---
+++ 20 Challenge 2 - The Simon Game/game.js
@@ -0,0 +1,10 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  console.log(gamePattern);
+}
+
